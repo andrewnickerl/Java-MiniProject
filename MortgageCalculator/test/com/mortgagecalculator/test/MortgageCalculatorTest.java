c6551074b84e0a8f2982d5fed4234e3cabd87190
Everett <62901530+ejstrunk@users.noreply.github.com>
2020-08-03 10:47:47 -0700

    Setup imports in test class

--- MortgageCalculator/test/com/mortgagecalculator/test/MortgageCalculatorTest.java
+++ MortgageCalculator/test/com/mortgagecalculator/test/MortgageCalculatorTest.java
@@ -1,5 +1,23 @@
 package com.mortgagecalculator.test;
 
+import com.mortgagecalculator.LoanType;
+import com.mortgagecalculator.Mortgage;
+import com.mortgagecalculator.MortgageCalculator;
+import jdk.jfr.StackTrace;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class MortgageCalculatorTest {
 
+    @Before
+    public void setUp() {
+        Mortgage mortgage = new Mortgage(23.0, 32.0, LoanType.CONVENTIONAL15, (float) .5, 12);
+    }
+
+    @Test
+    public void totalMonthlyPrice() {
+        assertTrue();
+    }
 }
