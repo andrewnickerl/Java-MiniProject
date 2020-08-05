f37731035e561e70212c3fdc5db99ac515d0a982
Andrew Nickerl <andrew.nickerl@gmail.com>
2020-08-05 11:52:20 -0700

    Added a client class and made CalculatorUI implement runnable so that our main method isn't nasty and long

---
+++ MortgageCalculator/src/com/mortgagecalculator/ui/CalculatorClient.java
@@ -0,0 +1,8 @@
+package com.mortgagecalculator.ui;
+
+public class CalculatorClient {
+    public static void main(String[] args) {
+        CalculatorUI ui = new CalculatorUI();
+        ui.run();
+    }
+}
